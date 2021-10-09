50559260ca8bc2b6382bec04ea972a28895aa6c7
thorisho18 <59007151+thorisho18@users.noreply.github.com>
2021-10-09 11:33:21 +0200

    Add files via upload

---
+++ slick.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
+$('#carousel-example').on('slide.bs.carousel', function (e) {
+    /*
+        CC 2.0 License Iatek LLC 2018 - Attribution required
+    */
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 5;
+    var totalItems = $('.carousel-item').length;
+ 
+    if (idx >= totalItems-(itemsPerSlide-1)) {
+        var it = itemsPerSlide - (totalItems - idx);
+        for (var i=0; i<it; i++) {
+            // append slides to end
+            if (e.direction=="left") {
+                $('.carousel-item').eq(i).appendTo('.carousel-inner');
+            }
+            else {
+                $('.carousel-item').eq(0).appendTo('.carousel-inner');
+            }
+        }
+    }
+const clip = document.querySelectorAll('.img-fluid');
+
+for (let i = 0 ; i <clip.length; i++){
+    clip[i].addEventListener('mouseenter',function(e){
+        clip[i].play()
+    })
+    clip[i].addEventListener('mouseout',function(e){
+        clip[i].pause()
+    })
+}
